e88292c28267159231a42ddea3754daf81b9a228
htsenyasa <htsenyasa@gmail.com>
2017-10-24 16:40:21 +0300

    generate potential

--- Src/xmds2/sample.py
+++ Src/xmds2/sample.py
@@ -1,10 +1,18 @@
 import shlex, subprocess, time
+import random as rnd
 
 
-#cmdline = "./xgp1d"
-cmdline = "./xgp1d --interaction_param=0 --num_particles=1 --freq=-0.5 --shift=10"
+inter_param = 0
+num_particles = 1
+freq = 1.5  # corresponds to omega in xmds file and it will change between [0, 1.7]
+shift = 0   # shift [-10, 10]
+
+#rnd.seed(10)
+shift = rnd.triangular(-10, 10)
+freq = rnd.triangular(0, 1.7)
+
+cmdline = "./xgp1d --interaction_param={} --num_particles={} --freq={} --shift={}".format(inter_param, num_particles, freq, shift)
 args = shlex.split(cmdline)
-print(args)
 p = subprocess.Popen(args)
 p.wait()
 
@@ -12,3 +20,7 @@ cmdline = "python2.7 gp1d.py"
 args = shlex.split(cmdline)
 p = subprocess.Popen(args)
 p.wait()
+
+
+#cmdline = "./xgp1d"
+#cmdline = "./xgp1d --interaction_param=0 --num_particles=1 --freq=0.5 --shift=10"
