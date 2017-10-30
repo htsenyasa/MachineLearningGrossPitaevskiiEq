1260355ec53faf50ce8a401f59c308c25036dc09
Levent Subasi <subasilevent@gmail.com>
2017-10-30 18:10:51 +0300

    gp1d.xmds: calculate Ekin, Epot, Eint separately. Check chem. pot. for 1D. Check virial for harmonic pot and shift 0.

--- Src/xmds2/gp1d.py
+++ Src/xmds2/gp1d.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python2.7
-
-import sys
-sys.path.insert(0, "/home/user/Desktop/Study/xmds/xmds-2.2.3/xpdeint")
-import matplotlib.pyplot as plt
-import numpy as np
-
+#!/usr/bin/env python
 from xpdeint.XSILFile import XSILFile
 
 xsilFile = XSILFile("gp1d.xsil")
@@ -22,26 +16,14 @@ phiI_1 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[0].dependen
 t_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].independentVariables if _["name"] == "t")
 norm_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "norm")
 e1_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "e1")
+e1kin_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "e1kin")
+e1pot_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "e1pot")
+e1int_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "e1int")
+vir1_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "vir1")
+mu1_2 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[1].dependentVariables if _["name"] == "mu1")
 x_3 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[2].independentVariables if _["name"] == "x")
 v1_3 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[2].dependentVariables if _["name"] == "v1")
 
-v1_3 = v1_3.reshape(1, len(v1_3))
-f = open("potential.dat", "a")
-f2 = open("energy.dat", "a")
-np.savetxt(f, v1_3)
-np.savetxt(f2, e1_2[-2:-1])
+# Write your plotting commands here.
+# You may want to import pylab (from pylab import *) or matplotlib (from matplotlib import *)
 
-
-
-#np.savetxt(f, np.concatenate((v1_3, e1_2[-2:-1])).reshape(1, np.size(v1_3)+1))
-
-#plt.plot(x_1, dens_1[-1,:], '.')
-#plt.plot(x_1, v1_3)
-
-#fig, ax = plt.subplots(1,2,figsize=(18,6))
-#ax[0].plot(x_1,dens_1[-1,:],'b.-', x_1, v1_3[:]/100., 'k-')
-
-#plt.show()
-#print e1_2[-2:-1]
-#print len(v1_3)
-#print v1_3
