7509ee1d74883978333cafbe2617053ed859daa6
htsenyasa <htsenyasa@gmail.com>
2017-12-09 18:26:53 +0300

    Problem statement and training result sections are updated

---
+++ figs/CNN/changedots.py
@@ -0,0 +1,16 @@
+import glob
+import sys
+import os
+sys.path.append("../../Src/")
+
+import analyzer as an
+
+files = []
+file_ex = "*.svg"
+
+for file in glob.glob(file_ex):
+    files.append(file)
+
+for src in files:
+    dst = src.replace("0.", "0_", 1)
+    os.rename(src, dst)
