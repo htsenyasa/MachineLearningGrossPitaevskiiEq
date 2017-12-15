0ec0baeb62f484e660ee846c7c7290860f55e335
htsenyasa <htsenyasa@gmail.com>
2017-12-15 20:29:05 +0300

    report update

--- figs/CNN/reproducesvg.py
+++ figs/CNN/reproducesvg.py
@@ -1,9 +1,28 @@
 import glob
 import sys
 import os
+import matplotlib.pyplot as plt
 sys.path.append("../../Src/")
 
 import analyzer as an
+epochs = [i+1 for i in range(60)]
+
+def plot_loss(info, file):
+    plt.plot(epochs, info.loss, "--b", label = "Loss")
+    plt.xlabel("Epochs", fontsize=18)
+    plt.ylabel("Loss", fontsize=18)
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    plt.grid()
+    plt.legend()
+    #plt.ylim(1e-3, 1e-1)
+    #plt.show()
+    plt.tick_params(labelsize = 12)
+    figure = plt.gcf()
+    figure.set_size_inches(8,6)
+    plt.savefig(file + "LOSS-" +".svg", format = "svg", dpi=1200)
+    plt.clf()
+
+
 
 files = []
 file_ex = "*.inf"
@@ -13,5 +32,7 @@ for file in glob.glob(file_ex):
 files.sort()
 
 for file in files:
-    plt = an.load_info(file + ".inf")
-    plt.display_plot(file)
+    info = an.load_info(file + ".inf")
+    #info.display_plot(file)
+    if(file.find("epoch-60") != -1):
+        plot_loss(info, file)
