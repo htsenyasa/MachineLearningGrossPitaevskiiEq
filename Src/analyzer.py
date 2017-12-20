a5c28becd7140309ce5ca626bcabce6b47700b1e
htsenyasa <htsenyasa@gmail.com>
2017-12-20 20:33:50 +0300

    energy dists are added

--- Src/analyzer.py
+++ Src/analyzer.py
@@ -32,12 +32,45 @@ class analyzer(object):
         self.error = (self.error/self.test_len)
         self.relative_error = sum(np.abs(self.predicted - self.testdataset) / sum(self.testdataset))
 
+#    def display_plot(self, file_name = None):
+#
+#        err = self.testdataset - self.predicted
+#        fig, ax1 = plt.subplots()
+#        left, bottom, width, height = [0.65, 0.20, .2, .2]
+#        inset = fig.add_axes([left, bottom, width, height])
+#        ax1.plot(self.testdataset, self.testdataset, "--r", label="real data", linewidth = 3)
+#        ax1.plot(self.testdataset, self.predicted, ".", label = "predicted", markersize = 2)
+#        ax1.set_title("FNN{}".format(self.arch))
+#        ax1.set_xlabel("Real $\\mu$", fontsize = 18)
+#        ax1.set_ylabel("Predicted", fontsize = 18)
+#        ax1.tick_params(labelsize = 12)
+#        ax1.legend()
+#        ax1.grid()
+#        props = dict(boxstyle='square', facecolor='white', alpha=0.5)
+#        textstr = "MSE:{:.4E}\nTraining Length:{}\nEpoch:{}\nBatch:{}".format(Decimal(float(self.error)), self.training_len, self.cur_epoch, self.batch_size)
+#        ax1.text(0.03, 0.85, textstr, transform=ax1.transAxes, fontsize=11, verticalalignment='top', bbox=props)
+#        #ax1.text(, , "{}\nlr={}\nepoch={}\ntrain_len={}\ntest_len={}\nerror={}".format(self.arch, self.learning_rate, self.cur_epoch, self.training_len, self.test_len, self.error))
+#
+#        inset.hist(err, range=[-np.amax(np.abs(err)), np.amax(np.abs(err))], bins=20)
+#        inset.set_title("Error")
+#
+#        if file_name == None:
+#            plt.show()
+#        else:
+#            figure = plt.gcf()
+#            figure.set_size_inches(8,6)
+#            plt.savefig(file_name + ".svg", format = "svg", dpi=1200)
+#            plt.clf()
+
     def display_plot(self, file_name = None):
 
         err = self.testdataset - self.predicted
+        relative_err = ((self.testdataset - self.predicted) / sum(self.testdataset)) * 100
         fig, ax1 = plt.subplots()
         left, bottom, width, height = [0.65, 0.20, .2, .2]
         inset = fig.add_axes([left, bottom, width, height])
+        left2, bottom2, width2, height2 = [0.20, 0.65, .2, .2]
+        inset2 = fig.add_axes([left2, bottom2, width2, height2])
         ax1.plot(self.testdataset, self.testdataset, "--r", label="real data", linewidth = 3)
         ax1.plot(self.testdataset, self.predicted, ".", label = "predicted", markersize = 2)
         ax1.set_title("FNN{}".format(self.arch))
@@ -52,7 +85,10 @@ class analyzer(object):
         #ax1.text(, , "{}\nlr={}\nepoch={}\ntrain_len={}\ntest_len={}\nerror={}".format(self.arch, self.learning_rate, self.cur_epoch, self.training_len, self.test_len, self.error))
 
         inset.hist(err, range=[-np.amax(np.abs(err)), np.amax(np.abs(err))], bins=20)
+        inset2.hist(relative_err, range=[-np.amax(np.abs(relative_err)), np.amax(np.abs(relative_err))], bins=20)
         inset.set_title("Error")
+        inset2.set_title("Relative Error")
+
 
         if file_name == None:
             plt.show()
@@ -61,6 +97,8 @@ class analyzer(object):
             figure.set_size_inches(8,6)
             plt.savefig(file_name + ".svg", format = "svg", dpi=1200)
             plt.clf()
+
+
 
 
     def display_plot2(self, file_name = None):
