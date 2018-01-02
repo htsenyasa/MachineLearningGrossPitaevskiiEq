50feca64df029c387acd504822935361388a84d6
htsenyasa <htsenyasa@gmail.com>
2018-01-02 11:16:53 +0300

    update

--- figs/FFNMERGED/sample.py
+++ figs/FFNMERGED/sample.py
@@ -13,15 +13,18 @@ def display_plot2(info, file_name = None):
     relative_err = []
     for i in range(len(features)):
         err.append(info.testdataset[:,i] - info.predicted[:,i])
-        relative_err.append(((info.testdataset[:,i] - info.predicted[:,i]) / (info.testdataset[:,i])) * 100)
+        relative_err.append(((info.testdataset[:,i] - info.predicted[:,i]) / (1e-15)) * 100)
         fig, ax1 = plt.subplots(num=features[i])
         left, bottom, width, height = [0.65, 0.20, .2, .2]
-        inset = fig.add_axes([left, bottom, width, height])
+        if i != 0: inset = fig.add_axes([left, bottom, width, height])
         left2, bottom2, width2, height2 = [0.19, 0.60, .2, .2]
         if i != 0: inset2 = fig.add_axes([left2, bottom2, width2, height2])
-        if i == 0: ax1.set_ylim(-3e-15, -3e-15)
+        marker_s = 2
+        if i == 0:
+            ax1.set_ylim(-0.1, 0.1)
+            marker_s = 10
         ax1.plot(info.testdataset[:,i], info.testdataset[:,i], "--r", linewidth = 3)
-        ax1.plot(info.testdataset[:,i], info.predicted[:,i], ".", markersize = 2)
+        ax1.plot(info.testdataset[:,i], info.predicted[:,i], ".", markersize = marker_s)
         #ax1.set_title("FNN{}".format(info.arch))
         ax1.set_xlabel("True " + features_l[i], fontsize = 20)
         ax1.set_ylabel("Predicted " + features_l[i], fontsize = 20)
@@ -31,17 +34,16 @@ def display_plot2(info, file_name = None):
         #props = dict(boxstyle='square', facecolor='white', alpha=0.5)
         #textstr = "MSE:{:.4E}\nTraining Length:{}\nEpoch:{}\nBatch:{}".format(Decimal(float(info.error[i])), info.training_len, info.cur_epoch, info.batch_size)
         #ax1.text(0.03, 0.85, textstr, transform=ax1.transAxes, fontsize=11, verticalalignment='top', bbox=props)
-        inset.hist(err[i], range=[-np.amax(np.abs(err[i])), np.amax(np.abs(err[i]))], bins=20)
-        inset.set_title("Error")
-        inset.tick_params(labelsize=12)
         if i != 0:
+            inset.hist(err[i], range=[-np.amax(np.abs(err[i])), np.amax(np.abs(err[i]))], bins=20)
+            inset.set_title("Error")
+            inset.tick_params(labelsize=12)
             inset2.hist(relative_err[i], range=[-np.amax(np.abs(relative_err[i])), np.amax(np.abs(relative_err[i]))], bins=20)
             inset2.set_title("Rel.Error (%)", fontsize = 18)
             inset2.tick_params(labelsize=12)
-        plt.show()
-#        plt.clf()
-#    plt.savefig(file_name + ".svg", format = "svg", dpi=1200)
-#    plt.clf()
+#        plt.show()
+        fig.savefig(file_name + "{}-".format(features[i]) + ".svg", format = "svg")
+        fig.clf()
 
 files = ["potential-g-0-epoch-60-"]
 
