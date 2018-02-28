e9d225cd5cb2ec78080638ddecac24bcada635cc
Levent Subasi <subasilevent@gmail.com>
2018-02-28 18:38:55 +0300

    fixed kinetic energy in FT basis='x y'

--- Src/xmds2_2d/gp2d.py
+++ Src/xmds2_2d/gp2d.py
@@ -1,32 +1,5 @@
 #!/usr/bin/env python
-import numpy as np
-import argparse
-import matplotlib.pyplot as plt
-import sys
-
-sys.path.append("/home/user/Desktop/")
 from xpdeint.XSILFile import XSILFile
-
-
-parser = argparse.ArgumentParser(description='xmds data')
-parser.add_argument('--pos-file-ex',         type=str,             default="-test.dat",                    help = 'target file ex')
-parser.add_argument('--dir',                 type=str,             default="",                             help = 'target file ex')
-parser.add_argument('--pot',                 type=str,             default="0",                              help = 'target file ex')
-
-
-args = parser.parse_args()
-
-file_ex = args.pos_file_ex
-dire = args.dir
-pot = args.pot
-root = "../../data/nonlinearSE/generic_dataset_2d/" + dire + "/"
-pot_file_name = root + "potential" + file_ex
-en_file_name = root + "energy" + file_ex
-ekin_file_name = root + "ekin" + file_ex
-epot_file_name = root + "epot" + file_ex
-eint_file_name = root + "eint" + file_ex
-dens_file_name = root + "dens" + file_ex
-
 
 xsilFile = XSILFile("gp2d.xsil")
 
@@ -54,21 +27,3 @@ dens_3 = firstElementOrNone(_["array"] for _ in xsilFile.xsilObjects[2].dependen
 # Write your plotting commands here.
 # You may want to import pylab (from pylab import *) or matplotlib (from matplotlib import *)
 
-
-#f = open(pot_file_name, "ab")
-#f2 = open(en_file_name, "ab")
-#f3 = open(ekin_file_name, "ab")
-#f4 = open(epot_file_name, "ab")
-#f5 = open(eint_file_name, "ab")
-#f6 = open(dens_file_name, "ab")
-#
-#pot = np.array(Pot_2)
-#np.save(f, Pot_2[-1])
-#
-#
-#np.save(f, Pot_2[-1])
-#np.save(f2, En_1)
-#np.save(f3, e1kin_2[-2:-1])
-#np.save(f4, e1pot_2[-2:-1])
-#np.save(f5, e1int_2[-2:-1])
-#np.save(f6, dens)
