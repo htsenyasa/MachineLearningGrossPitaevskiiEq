eb484e0486052bda6312f678d6c902ba38093dc3
htsenyasa <htsenyasa@gmail.com>
2018-03-29 17:16:57 +0300

    Implementation of new random generator mentioned in SE&ML article

--- Src/xmds2/potentialgenerator.py
+++ Src/xmds2/potentialgenerator.py
@@ -9,7 +9,7 @@ class PotentialGenerator(object):
     cur_pot_type = None
     type_of_pots = 0
 
-    def __init__(self, seed = 100, width = 10, Np = 128, inf_val = 5, g_exec_func = None):
+    def __init__(self, seed = 100, width = 10, Np = 128, inf_val = 30, g_exec_func = None):
         self.width = width
         self.total_width = 2 * width
         self.step_size = self.total_width / float(Np)
@@ -26,7 +26,6 @@ class PotentialGenerator(object):
 
         return envelope
 
-
     def potential_process(self, pot, procs = [], args = []):
         """ args: A list of len(procs) that involves arguments of procs as tuples"""
         Nprocs = len(procs)
@@ -40,7 +39,6 @@ class PotentialGenerator(object):
         pot += np.abs(np.min(pot))
 
         return pot
-
 
     def generate_random_pot(self, sigma = 3, exec_func = None):
         c = np.array([rnd.gauss(0, 1) for i in range(self.Np)])
@@ -61,7 +59,6 @@ class PotentialGenerator(object):
             exec_func(self.x, pot)
 
         return pot
-    
 
     def generate_random_pot_2(self, sigma = None, exec_func = None):
         """ Create random potential by using sine and cosine series with random coeffs. """
@@ -92,6 +89,35 @@ class PotentialGenerator(object):
 
         return pot
 
+    def generate_random_pot_3(self, sigma = None, scale_fac = 8, exec_func = None):
+        if sigma == None:
+            sigma = rnd.uniform(3, 10)
+
+        bin_grid = np.array([rnd.randint(0, 1) for i in range(self.Np // scale_fac)])
+        bin_grid = np.repeat(bin_grid, scale_fac)
+
+        bin_grid_2 = np.array([rnd.randint(0, 1) for i in range(self.Np // (scale_fac * 2))])
+        bin_grid_2 = np.repeat(bin_grid_2, scale_fac)
+
+        chunk = np.zeros((len(bin_grid) - len(bin_grid_2)) // 2)
+
+        bin_grid_2 = np.concatenate((chunk, bin_grid_2), axis = 0)
+        bin_grid_2 = np.concatenate((bin_grid_2, chunk), axis = 0)
+
+        pot = bin_grid - bin_grid_2
+
+        procs = [scipy.ndimage.filters.gaussian_filter1d]
+        args = [(pot, sigma)]
+
+        pot = self.potential_process(pot, procs, args)
+
+        if self.g_exec_func != None:
+            self.g_exec_func(self.x, pot)
+
+        if exec_func != None:
+            exec_func(self.x, pot)    
+
+        return pot
 
     def generate_well_pot(self, exec_func = None):
         lc = rnd.uniform(-5, 5) 
@@ -111,7 +137,6 @@ class PotentialGenerator(object):
 
         return pot
 
-
     def generate_harmonic_pot(self, exec_func = None):
         x0 = rnd.uniform(-5, 5)
         omega = rnd.uniform(0.01, 4)
@@ -128,7 +153,6 @@ class PotentialGenerator(object):
             exec_func(self.x, pot)    
 
         return pot
-
 
     def generate_gaussian_pot(self, exec_func = None):
         low = 0
